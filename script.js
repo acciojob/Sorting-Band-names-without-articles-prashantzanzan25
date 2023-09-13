8a85d6100c5bcf3ed5c5afafcd395c4d1c4c16eb
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-09-13 12:25:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-//your code here
-let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'The Beatles', 'Pink Floyd', 'Metallica'];
-function removeArticles(name) {
-    const articleRegex = /^(a|an|the)\s+/i;
-    return name.replace(articleRegex, '');
-}
+    let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'The Beatles', 'Aerosmith', 'The Who'];
 
-bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+    // Function to sort band names without articles
+    function sortBandNamesWithoutArticles(names) {
+      return names.sort((a, b) => {
+        const articles = ['a', 'an', 'the'];
+        const getNameWithoutArticle = (name) => {
+          for (const article of articles) {
+            if (name.toLowerCase().startsWith(article + ' ')) {
+              return name.slice(article.length + 1); // Add 1 to account for the space after the article
+            }
+          }
+          return name;
+        };
+        return getNameWithoutArticle(a).localeCompare(getNameWithoutArticle(b));
+      });
+    }
 
+    // Sort the band names without articles
+    bandNames = sortBandNamesWithoutArticles(bandNames);
 
-const bandList = document.getElementById('band');
+    // Create the ul element with id 'bands'
+    const ul = document.getElementById('bands');
 
-
-for (const bandName of bandNames) {
-    const listItem = document.createElement('li');
-    listItem.textContent = bandName;
-    bandList.appendChild(listItem);
-}
-
+    // Loop through the sorted band names and create li elements
+    bandNames.forEach((name) => {
+      const li = document.createElement('li');
+      li.textContent = name;
+      ul.appendChild(li);
+    });
