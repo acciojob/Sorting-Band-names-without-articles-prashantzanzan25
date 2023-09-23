aa431b160c72a8d599dc59131f8dd04efc952eeb
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-09-23 14:05:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+document.addEventListener('DOMContentLoaded', function () {
+  const bands = [
+    'The Rolling Stones',
+    'Led Zeppelin',
+    'The Who',
+    'Pink Floyd',
+    'The Beatles',
+    'Radiohead',
+  ];
 
-// So here the lexicographically sorted order is (without articles)---
-touristSpots = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
+  // Function to remove articles from a band name for sorting
+  function removeArticles(name) {
+    return name.replace(/^(a |an |the )/i, '').trim();
+  }
+
+  // Sort the bands without articles
+  const sortedBands = bands.slice().sort((a, b) => {
+    const bandA = removeArticles(a);
+    const bandB = removeArticles(b);
+    return bandA.localeCompare(bandB);
+  });
+
+  // Get the ul element by its id
+  const ul = document.getElementById('bands');
+
+  // Clear any existing items in the ul
+  ul.innerHTML = '';
+
+  // Create and append li elements for each sorted band name
+  sortedBands.forEach((band) => {
+    const li = document.createElement('li');
+    li.textContent = band;
+    ul.appendChild(li);
+  });
+});
