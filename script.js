1f7d18e5dcc271ba362f048df4cdaa92f90ee8ea
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-10-12 19:02:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const bands = [
-    'The Rolling Stones',
-    'Led Zeppelin',
-    'The Who',
-    'Pink Floyd',
-    'The Beatles',
-    'Radiohead',
-  ];
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
-  // Function to remove articles from a band name for sorting
-  function removeArticles(name) {
-    return name.replace(/^(a |an |the )/i, '').trim();
-  }
+// Function to remove articles ('a', 'an', 'the') and return the cleaned name
+function cleanName(name) {
+  return name.replace(/^(a|an|the) /i, '');
+}
 
-  // Sort the bands without articles
-  const sortedBands = bands.slice().sort((a, b) => {
-    const bandA = removeArticles(a);
-    const bandB = removeArticles(b);
-    return bandA.localeCompare(bandB);
-  });
+// Sort the array without articles
+touristSpots.sort((a, b) => cleanName(a).localeCompare(cleanName(b)));
 
-  // Get the ul element by its id
-  const ul = document.getElementById('bands');
+// Create an HTML list
+const ul = document.createElement('ul');
+ul.id = 'band';
 
-  // Clear any existing items in the ul
-  ul.innerHTML = '';
-
-  // Create and append li elements for each sorted band name
-  sortedBands.forEach((band) => {
-    const li = document.createElement('li');
-    li.textContent = band;
-    ul.appendChild(li);
-  });
+touristSpots.forEach(bandName => {
+  const li = document.createElement('li');
+  li.textContent = bandName;
+  ul.appendChild(li);
 });
+
+// Add the list to the document
+document.getElementById('band').replaceWith(ul);
